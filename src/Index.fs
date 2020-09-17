19e8a26fe8a759d99de8e145225983b5cb3f4153
Oliver Coad <oliver.coad@gmail.com>
2020-09-17 20:02:17 +1000

    Enable debugger

--- src/Index.fs
+++ src/Index.fs
@@ -9,9 +9,14 @@ open App
 
 #if DEBUG
 printfn "Enabled HMR"
+printfn "Enabled Debugger"
 open Elmish.HMR
+open Elmish.Debug
 #endif
 
 Program.mkProgram init update view
 |> Program.withReactSynchronous "elmish-app"
+#if DEBUG
+|> Program.withDebugger
+#endif
 |> Program.run
