156a4e9b7d6e7c578d9a8efaeb89189b31b230c0
Oliver Coad <oliver.coad@gmail.com>
2021-03-26 06:25:12 +1100

    Move shareContent into file

--- src/App.fs
+++ src/App.fs
@@ -13,6 +13,7 @@ open Utils
 
 open FancyButton
 open SliderMorph
+open Share
 
 let videoDim = 512
 let imgDim = 300
@@ -71,15 +72,6 @@ let pageDescription = function
             sprintf "Morph generated faces from %s to %s" fromValue toValue
     | None ->
             "Generate faces on the fly and morph between them"
-
-type ShareMsg =
-    | OpenShare
-    | CloseShare
-    | CopyLink
-    | SetShareMsg of string
-    | ResetShareMsg
-    | NavigatorShare
-
 type Msg =
     | SetLeftValue of string
     | SetRightValue of string
@@ -380,86 +372,13 @@ let renderContent (state:State) (dispatch: Msg -> unit) =
         ]
     ]
 
-let shareContent state dispatch =
-        let encodedUrl = encodeUriComponent (canonicalUrl state)
-        let shareButtons = [
-            "Facebook", facebookIcon [ ], sprintf "https://www.facebook.com/sharer.php?display=page&u=%s&hashtag=%%23facemorph" encodedUrl
-            "WhatsApp", whatsAppIcon [ ], sprintf "https://api.whatsapp.com/send?text=%s" encodedUrl
-            "Twitter", twitterIcon [ ], sprintf "https://twitter.com/share?url=%s&hashtags=facemorph" encodedUrl
-            "Reddit", redditIcon [ ], sprintf "https://reddit.com/submit?url=%s" encodedUrl
-        ]
-        let isShown = state.VidValues.IsSome
-        Html.div [
-            prop.style [
-                style.textAlign.right
-                style.padding (length.em 2)
-                style.overflow.hidden // slides in from off screen
-
-                //use clamp as alternative to media query to remove whitespace except on small witdh
-                style.custom ("marginBottom", "clamp(-2em - 45px, 5 * (700px - 100%), 0px)")
-            ]
-            prop.children [
-                Mui.slide [
-                    slide.in' isShown
-                    slide.direction.left
-                    prop.custom ("exit", false) //disable slide on exit
-                    slide.children (
-                        Html.div [
-                            Html.p [
-                                prop.style [ style.marginBottom (length.px 10) ]
-                                let message =
-                                    state.ShareLinkMsg
-                                    |> Option.defaultValue "Share this morph"
-                                prop.text message
-                            ]
-                            Mui.tooltip [
-                                tooltip.title ("Copy link")
-                                tooltip.placement.bottomEnd
-                                tooltip.children (
-                                    Mui.speedDial [
-                                        speedDial.icon (
-                                            Mui.speedDialIcon [
-                                                speedDialIcon.icon (shareIcon [ ])
-                                                speedDialIcon.openIcon (fileCopyIcon [ ])
-                                            ]
-                                        )
-                                        speedDial.ariaLabel "Share this morph"
-                                        speedDial.children [
-                                            for name, icon, href in shareButtons do
-                                                Mui.speedDialAction [
-                                                    speedDialAction.icon icon
-                                                    prop.key name
-                                                    speedDialAction.tooltipTitle name
-                                                    speedDialAction.tooltipPlacement.bottom
-                                                    speedDialAction.FabProps [
-                                                        fab.href href
-                                                        prop.target "_blank"
-                                                        prop.rel.noopener
-                                                    ]
-                                                ]
-                                        ]
-                                        speedDial.open' state.ShareOpen
-                                        speedDial.onOpen (fun _ _ -> dispatch OpenShare)
-                                        speedDial.onClose (fun _ _ -> dispatch CloseShare)
-                                        speedDial.direction.left
-                                        speedDial.FabProps [
-                                            prop.onClick (fun e ->
-                                                e.preventDefault()
-                                                if state.ShareOpen
-                                                then dispatch CopyLink
-                                                elif navigatorCanShare
-                                                then dispatch NavigatorShare
-                                                else ()
-                                            )
-                                        ]
-                                    ]
-                                )
-                            ]
-                        ]
-                    )
-                ]
-            ]
-        ]
+let getShareState state =
+    { 
+        IsOpen = state.ShareOpen
+        LinkMsg = state.ShareLinkMsg
+        CanonicalUrl = canonicalUrl state
+        IsShown = state.VidValues.IsSome
+    }
 
 let header =
     Html.header [
@@ -572,7 +491,7 @@ let render (state:State) (dispatch: Msg -> unit) =
         Mui.cssBaseline [ ]
         header
         renderContent state dispatch
-        shareContent state (ShareMsg >> dispatch)
+        viewShareContent (getShareState state) (ShareMsg >> dispatch)
         Explain.view ()
         footer
     ]
